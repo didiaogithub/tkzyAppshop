2e437f61dc77a6925d68315ab1484ea3217e5632
王博 <wangbo3919@163.com>
2018-05-23 14:40:16 +0800

    修改代码

--- appmall/Pods/Target Support Files/Pods-appmall/Pods-appmall-resources.sh
+++ appmall/Pods/Target Support Files/Pods-appmall/Pods-appmall-resources.sh
@@ -100,6 +100,10 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/JGProgressHUD/JGProgressHUD/Resources/jg_hud_success@2x.png"
   install_resource "${PODS_ROOT}/JGProgressHUD/JGProgressHUD/Resources/jg_hud_success@3x.png"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/SobotKit.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/ZCEmojiExpression.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/en.lproj"
+  install_resource "${PODS_ROOT}/SobotKit/zh-Hans.lproj"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
@@ -110,6 +114,10 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/JGProgressHUD/JGProgressHUD/Resources/jg_hud_success@2x.png"
   install_resource "${PODS_ROOT}/JGProgressHUD/JGProgressHUD/Resources/jg_hud_success@3x.png"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/SobotKit.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/ZCEmojiExpression.bundle"
+  install_resource "${PODS_ROOT}/SobotKit/en.lproj"
+  install_resource "${PODS_ROOT}/SobotKit/zh-Hans.lproj"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"

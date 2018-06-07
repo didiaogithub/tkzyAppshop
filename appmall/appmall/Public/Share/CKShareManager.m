9ae5506d0bd3ca8ae3b267f58ffa3975da733e38
majun <1078231019@qq.com>
2018-06-07 13:54:34 +0800

    去除sharesdk

--- appmall/appmall/Public/Share/CKShareManager.m
+++ appmall/appmall/Public/Share/CKShareManager.m
@@ -7,16 +7,16 @@
 //
 
 #import "CKShareManager.h"
-#import <ShareSDK/ShareSDK.h>
-#import <ShareSDKUI/ShareSDK+SSUI.h>
-#import <ShareSDKConnector/ShareSDKConnector.h>
-//腾讯开放平台（对应QQ和QQ空间）SDK头文件
-#import <TencentOpenAPI/TencentOAuth.h>
-#import <TencentOpenAPI/QQApiInterface.h>
-//微信SDK头文件
-#import "WXApi.h"
-//新浪微博SDK头文件
-#import "WeiboSDK.h"
+//#import <ShareSDK/ShareSDK.h>
+//#import <ShareSDKUI/ShareSDK+SSUI.h>
+//#import <ShareSDKConnector/ShareSDKConnector.h>
+////腾讯开放平台（对应QQ和QQ空间）SDK头文件
+//#import <TencentOpenAPI/TencentOAuth.h>
+//#import <TencentOpenAPI/QQApiInterface.h>
+////微信SDK头文件
+//#import "WXApi.h"
+////新浪微博SDK头文件
+//#import "WeiboSDK.h"
 
 @implementation CKShareManager
 

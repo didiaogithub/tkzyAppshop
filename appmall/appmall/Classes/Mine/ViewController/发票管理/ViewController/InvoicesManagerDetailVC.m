92a9ad606a6b4967c379ca29b1e15c786bea58ed
majun <1078231019@qq.com>
2018-06-08 19:18:48 +0800

    发票修改

--- appmall/appmall/Classes/Mine/ViewController/发票管理/ViewController/InvoicesManagerDetailVC.m
+++ appmall/appmall/Classes/Mine/ViewController/发票管理/ViewController/InvoicesManagerDetailVC.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *invoicehead;
 @property (weak, nonatomic) IBOutlet UIImageView *invoiceImage;
 @property (weak, nonatomic) IBOutlet UIButton *downloadBtn;
+/**  path*/
+@property (nonatomic, strong) NSString *path;
 
 - (IBAction)downloadBtnAction:(UIButton *)sender;
 @end
@@ -45,7 +47,7 @@
             }
             self.invoicehead.text = dict[@"issuingoffice"];
             self.invoicecotent.text = dict[@"content"];
-            
+            self.path = dict[@"path"];
             
         }
         
@@ -62,6 +64,52 @@
 
 - (IBAction)downloadBtnAction:(UIButton *)sender {
     
+    [self.view addSubview:self.loadingView];
+    [self.loadingView startAnimation];
+    if (!IsNilOrNull(self.path)) {
+        [self toSaveImage:self.path];
+    }else{
+        [self.loadingView stopAnimation];
+        [self showNoticeView:@"发票下载链接是空"];
+    }
+}
+
+- (void)toSaveImage:(NSString *)urlString {
     
+    NSURL *url = [NSURL URLWithString: urlString];
+    SDWebImageManager *manager = [SDWebImageManager sharedManager];
+    UIImage *img;
+    if ([manager diskImageExistsForURL:url])
+    {
+        img =  [[manager imageCache] imageFromDiskCacheForKey:url.absoluteString];
+    }
+    else
+    {
+        //从网络下载图片
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        img = [UIImage imageWithData:data];
+    }
+    // 保存图片到相册中
+    UIImageWriteToSavedPhotosAlbum(img,self, @selector(image:didFinishSavingWithError:contextInfo:),nil);
+    
+}
+//保存图片完成之后的回调
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error
+  contextInfo:(void *)contextInfo
+{
+    // Was there an error?
+    if (error != NULL)
+    {
+        [self.loadingView stopAnimation];
+        // Show error message…
+        [self showNoticeView:@"图片保存失败"];
+        
+    }
+    else  // No errors
+    {
+        [self.loadingView stopAnimation];
+        // Show message image successfully saved
+        [self showNoticeView:@"图片保存成功"];
+    }
 }
 @end
--- appmall/appmall/Public/Share/CKShareManager.m
+++ appmall/appmall/Public/Share/CKShareManager.m
@@ -38,7 +38,6 @@
     return self;
 }
 
-<<<<<<< HEAD
 -(void)configShare {
     //分享
     /**
@@ -101,120 +100,7 @@
 
 
 //    [ShareSDK registerApp:shareSDKAppID
-=======
-//-(void)configShare {
-//    //分享
-//    /**
-//     *  设置ShareSDK的appKey，如果尚未在ShareSDK官网注册过App，请移步到http://mob.com/login 登录后台进行应用注册
-//     *  在将生成的AppKey传入到此方法中。
-//     *  方法中的第二个第三个参数为需要连接社交平台SDK时触发，
-//     *  在此事件中写入连接代码。第四个参数则为配置本地社交平台时触发，根据返回的平台类型来配置平台信息。
-//     *  如果您使用的时服务端托管平台信息时，第二、四项参数可以传入nil，第三项参数则根据服务端托管平台来决定要连接的社交SDK。 11891bf4882e0
-//     */
-//    [ShareSDK registerActivePlatforms:@[
-//                                       @(SSDKPlatformSubTypeWechatSession),//微信好友
-//                                       @(SSDKPlatformSubTypeWechatTimeline)//微信朋友圈
->>>>>>> 77b98c1eb7e0b3abac5c6760db5282bd2f2d5201
-//
-//                                       ]
-//onImport:^(SSDKPlatformType platformType) {
-//    switch (platformType)
-//    {
-//        case SSDKPlatformTypeWechat:
-//            [ShareSDKConnector connectWeChat:[WXApi class]];
-//            break;
-//
-//        case SSDKPlatformTypeQQ:
-//            [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
-//            break;
-//        case SSDKPlatformTypeSinaWeibo:
-//            [ShareSDKConnector connectWeibo:[WeiboSDK class]];
-//            break;
-//
-//
-//        default:
-//            break;
-//    }
-//                                       }
-//onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo) {
-//    switch (platformType){
-//        case SSDKPlatformTypeSinaWeibo:
-//            //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
-//            [appInfo SSDKSetupSinaWeiboByAppKey:weiBoAppkey
-//                                      appSecret:weiBoSecrete
-//                                    redirectUri:@"http://www.sharesdk.cn"
-//                                       authType:SSDKAuthTypeBoth];
-//            break;
-//        case SSDKPlatformTypeWechat:
-//            [appInfo SSDKSetupWeChatByAppId:kWXAPP_ID appSecret:kWXAPP_SECRET];
-//
-//            break;
-//        case SSDKPlatformTypeQQ:
-//            [appInfo SSDKSetupQQByAppId:qqAppID
-//                                 appKey:qqAppKey
-//                               authType:SSDKAuthTypeBoth];
-//            break;
-//        case SSDKPlatformSubTypeQZone:
-//        default:
-//            break;
-//    }
-//}];
-//
-//
-////    [ShareSDK registerApp:shareSDKAppID
-////
-////          activePlatforms:@[@(SSDKPlatformTypeSinaWeibo),//新浪
-////                            @(SSDKPlatformTypeWechat),
-////                            @(SSDKPlatformTypeQQ),//QQ好友
-////                            @(SSDKPlatformSubTypeQZone),//空间
-////                            @(SSDKPlatformSubTypeWechatSession),//微信好友
-////                            @(SSDKPlatformSubTypeWechatTimeline),//微信朋友圈
-////                            @(SSDKPlatformSubTypeQQFriend)//QQ好友
-////                            ]
-////          onImport:^(SSDKPlatformType platformType){
-////                     switch (platformType)
-////                     {
-////                         case SSDKPlatformTypeWechat:
-////                             [ShareSDKConnector connectWeChat:[WXApi class]];
-////                             break;
-////
-////                         case SSDKPlatformTypeQQ:
-////                             [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
-////                             break;
-////                         case SSDKPlatformTypeSinaWeibo:
-////                             [ShareSDKConnector connectWeibo:[WeiboSDK class]];
-////                             break;
-////
-////
-////                         default:
-////                             break;
-////                     }
-////                 }
-////          onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo){
-////         switch (platformType){
-////                 case SSDKPlatformTypeSinaWeibo:
-////                     //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
-////                     [appInfo SSDKSetupSinaWeiboByAppKey:weiBoAppkey
-////                                               appSecret:weiBoSecrete
-////                                             redirectUri:@"http://www.sharesdk.cn"
-////                                                authType:SSDKAuthTypeBoth];
-////                     break;
-////                 case SSDKPlatformTypeWechat:
-////                     [appInfo SSDKSetupWeChatByAppId:kWXAPP_ID appSecret:kWXAPP_SECRET];
-////
-////                     break;
-////                 case SSDKPlatformTypeQQ:
-////                     [appInfo SSDKSetupQQByAppId:qqAppID
-////                                          appKey:qqAppKey
-////                                        authType:SSDKAuthTypeBoth];
-////                     break;
-////                 case SSDKPlatformSubTypeQZone:
-////                 default:
-////                     break;
-////             }
-////     }];
-//}
-
+}
 
 +(void)shareToFriendWithName:(NSString *)name andHeadImages:(id)headImages  andUrl:(NSURL *)url andTitle:(NSString*)title
 {

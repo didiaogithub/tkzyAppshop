9fb2e51f7a5d270950dd707cf6a6df14e05b7053
majun <1078231019@qq.com>
2018-06-22 19:55:46 +0800

    修改bug

--- appmall/appmall/Appdelegate/AppDelegate.m
+++ appmall/appmall/Appdelegate/AppDelegate.m
@@ -55,14 +55,24 @@
     
     //分享
     [CKShareManager manager];
-    
+    [self configKeyboard];
     [self initKeyWindow];
    
     [CKCNotificationCenter addObserver:self selector:@selector(checkAppDoesOnCheck) name:RequestManagerReachabilityDidChangeNotification object:nil];
      [WXApi registerApp:kWXAPP_ID];
-    [self configKeyboard];
     return YES;
 }
+
+/* -------------- 键盘管理 -------------*/
+-(void)configKeyboard{
+    IQKeyboardManager *manager = [IQKeyboardManager sharedManager];
+    manager.enable = YES; // 控制整个功能是否启用。
+    manager.shouldResignOnTouchOutside = YES; // 控制点击背景是否收起键盘
+    manager.shouldToolbarUsesTextFieldTintColor = YES; // 控制键盘上的工具条文字颜色是否用户自定义
+    manager.enableAutoToolbar = YES; // 控制是否显示键盘上的工具条
+    manager.toolbarManageBehaviour = IQAutoToolbarByTag; // 最新版的设置键盘的returnKey的关键字 ,可以点击键盘上的next键，自动跳转到下一个输入框，最后一个输入框点击完成，自动收起键盘。
+}
+
 
 + (AppDelegate* )shareAppDelegate {
     return (AppDelegate*)[UIApplication sharedApplication].delegate;
@@ -404,14 +414,7 @@
     }
 }
 
--(void)configKeyboard{
-    IQKeyboardManager *manager = [IQKeyboardManager sharedManager];
-    manager.enable = YES; // 控制整个功能是否启用。
-    manager.shouldResignOnTouchOutside = YES; // 控制点击背景是否收起键盘
-    manager.shouldToolbarUsesTextFieldTintColor = YES; // 控制键盘上的工具条文字颜色是否用户自定义
-    manager.enableAutoToolbar = YES; // 控制是否显示键盘上的工具条
-    manager.toolbarManageBehaviour = IQAutoToolbarByTag; // 最新版的设置键盘的returnKey的关键字 ,可以点击键盘上的next键，自动跳转到下一个输入框，最后一个输入框点击完成，自动收起键盘。
-}
+
 
 -(void)enterFirstPage {
     RootTabBarController *rootVC = [[RootTabBarController alloc]init];
